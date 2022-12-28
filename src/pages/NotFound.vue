0cd5088529b475ab190f8ba8bb867280b3f1e7d3
Dariusz Pilecki <pileckidariusz90@gmail.com>
2022-12-28 18:56:27 +0100

    not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,8 @@
 <template>
-    Not found
+    <section>
+        <base-card>
+            <h2>Page not found</h2>
+            <p>Page cannot be found. Maybe check out all our <router-link to="/coaches">coaches</router-link></p>
+        </base-card>
+    </section>
 </template>
